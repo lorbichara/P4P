7d011339eae8ddb5c589311790d7fbed4c2ecae9
lorbichara <lorbichara@gmail.com>
2019-02-04 11:32:33 -0600

    Example

--- example.c
+++ example.c
@@ -1,28 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <papi.h>
+//#include <papi.h>
 
 int main( int argc, char *argv[] )
 {
-	int i, j, k;
+	printf("hola");
+	// int i, j, k;
 
-	long long counters[3];
-	int PAPI_events[] = {
-			PAPI_TOT_CYC,
-			PAPI_L2_DCM,
-			PAPI_L2_DCA };
+	// long long counters[3];
+	// int PAPI_events[] = {
+	// 		PAPI_TOT_CYC,
+	// 		PAPI_L2_DCM,
+	// 		PAPI_L2_DCA };
 
-	PAPI_library_init(PAPI_VER_CURRENT);
-	i = PAPI_start_counters( PAPI_events, 3 );
+	// PAPI_library_init(PAPI_VER_CURRENT);
+	// i = PAPI_start_counters( PAPI_events, 3 );
 
-	/* your code here */
+	//  your code here 
 
-	PAPI_read_counters( counters, 3 );
+	// PAPI_read_counters( counters, 3 );
 
-	printf("%lld L2 cache misses (%.3lf%% misses) in %lld cycles\n", 
-		counters[1],(double)counters[1] / (double)counters[2],
-		counters[0] );
+	// printf("%lld L2 cache misses (%.3lf%% misses) in %lld cycles\n", 
+	// 	counters[1],(double)counters[1] / (double)counters[2],
+	// 	counters[0] );
 
 	return 0;
 }
