bf17098b7268dd0ef13155015211f0c4efc71a10
lorbichara <lorbichara@gmail.com>
2019-02-04 11:25:31 -0600

    Add initial example

---
+++ example.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <papi.h>
+
+int main( int argc, char *argv[] )
+{
+	int i, j, k;
+
+	long long counters[3];
+	int PAPI_events[] = {
+			PAPI_TOT_CYC,
+			PAPI_L2_DCM,
+			PAPI_L2_DCA };
+
+	PAPI_library_init(PAPI_VER_CURRENT);
+	i = PAPI_start_counters( PAPI_events, 3 );
+
+	/* your code here */
+
+	PAPI_read_counters( counters, 3 );
+
+	printf("%lld L2 cache misses (%.3lf%% misses) in %lld cycles\n", 
+		counters[1],(double)counters[1] / (double)counters[2],
+		counters[0] );
+
+	return 0;
+}
