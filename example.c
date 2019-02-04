ff977ebbe869b7c0228e6635dd49f90fb5cc8633
lorbichara <lorbichara@gmail.com>
2019-02-04 11:40:27 -0600

    modified

--- example.c
+++ example.c
@@ -1,11 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 //#include <papi.h>
+
+#define MAX_STRING_LEN 80
 
 int main( int argc, char *argv[] )
 {
-	printf("hola");
+	char S[MAX_STRING_LEN];
+	int l, i;
+	  
+	S[0] = 'a';
+	S[1] = 'b';
+	S[2] = 'c';
+	S[3] = 'd';
+	S[4] = 'e';
+	S[5] = 'g';
+	S[6] = '0';
+	S[7] = 0;
+
+	l = strlen(S);
+
+	printf("S:\t%s\n",S);
+	printf("length:\t%d\n",l);
+
+	/* print characters in S */
+
+	printf("forward\n");
+	for (i = 0; i < l; ++i) 
+		printf("A[%d] = %c\n",i,S[i]);
 	// int i, j, k;
 
 	// long long counters[3];
