c25036330becc0b80c8457f6a791eb3caa03759f
lorbichara <lorbichara@gmail.com>
2019-02-05 11:11:19 -0600

    Check HW counters

--- a1.c
+++ a1.c
@@ -1,35 +1,57 @@
 #include <papi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	double mat[2000][2000];
 	int d;
-
 	printf("Matrix dimension? ");
-	scanf("%d", d);
+	scanf("%d", &d);
 
+	// initialize matrix A with random doubles
 	for(int i = 0; i < d; i++)
 	{
 		for(int j = 0; j < d; j++)
 		{
-			mat[i][j] = rand();
+			matA[i][j] = (double)rand()/RAND_MAX*2.0-1.0;
 		}
 	}
 
+	// initialize matrix B with random doubles
 	for(int i = 0; i < d; i++)
 	{
 		for(int j = 0; j < d; j++)
 		{
-			printf("%lf \t", mat[i][j]);
+			matB[i][j] = (double)rand()/RAND_MAX*2.0-1.0;
+		}
+	}
+
+	double result[d][d]; // matrix that will store the result of the mult
+	
+	// matrix multiplication
+	for(int i = 0; i < d; i++)
+	{
+		for(int j = 0; j < d; j++)
+		{
+			result[i][j] = 0;
+			for(int k = 0; k < d; k++)
+			{
+				result[i][j] += matA[i][k] * matB[k][j];
+			}
+		}
+	}
+
+	// print result matrix
+	for(int i = 0; i < d; i++)
+	{
+		for(int j = 0; j < d; j++)
+		{
+			printf("%lf \t", result[i][j]);
 		}
 
 		printf("\n");
 	}
 
-
-
-
-
+	printf("Number of HW counters: %d", PAPI_num_counters());
 
 	return 0;
 }
