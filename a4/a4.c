0c47421de33a17ea66d9e727bdaf0e6c9387241f
lorbichara <lorbichara@gmail.com>
2019-03-28 21:53:33 -0500

    Measure flops for question 4

--- a4/a4.c
+++ a4/a4.c
@@ -394,10 +394,10 @@ void MMMCacheRegisterBlocking(int N)
 	PAPI_library_init(PAPI_VER_CURRENT);
 	int w = PAPI_start_counters(PAPI_events, 2);
 
-	// float real_time, proc_time, mflops;
-	// long long flpins;
-	// int execTime;
-	// execTime=PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
+	float real_time, proc_time, mflops;
+	long long flpins;
+	int execTime;
+	execTime=PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
 
 	for(int bj = 0; bj < N; bj+=NB)
 	{
@@ -443,11 +443,11 @@ void MMMCacheRegisterBlocking(int N)
 	}
 
 	//PAPI measurements
-	PAPI_read_counters(counters, 2);
-	printf("%lld L1 cache misses (%.3lf%% misses)\n", counters[0],(double)counters[0] / (double)counters[1]);
+	// PAPI_read_counters(counters, 2);
+	// printf("%lld L1 cache misses (%.3lf%% misses)\n", counters[0],(double)counters[0] / (double)counters[1]);
 
-	// execTime=PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
-	// printf("Mflops: %f\n", mflops*4*4);
+	execTime=PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
+	printf("Mflops: %f\n", mflops*4*4);
 	
 	PAPI_shutdown();
 
