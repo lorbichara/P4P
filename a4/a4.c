4626975eaf6394b15a2dfb4284bf77798991b0a5
lorbichara <lorbichara@gmail.com>
2019-03-28 23:05:54 -0500

    Flops for MKL

--- a4/a4.c
+++ a4/a4.c
@@ -676,19 +676,26 @@ void MMMMKL(int matrixSize)
 			:"%ebx","%ecx","%edx");
 
 	//PAPI measurements
-	long long counters[2];
-	int PAPI_events[] = {
-		PAPI_L1_DCM,
-		PAPI_L1_DCA
-	};
-	PAPI_library_init(PAPI_VER_CURRENT);
-	int w = PAPI_start_counters(PAPI_events, 2);
+	// long long counters[2];
+	// int PAPI_events[] = {
+	// 	PAPI_L1_DCM,
+	// 	PAPI_L1_DCA
+	// };
+	// PAPI_library_init(PAPI_VER_CURRENT);
+	// int w = PAPI_start_counters(PAPI_events, 2);
+	float real_time, proc_time, mflops;
+	long long flpins;
+	int execTime;
+	execTime=PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
 
 	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, matrixSize, matrixSize, matrixSize, alpha, A, matrixSize, B, matrixSize, beta, C, matrixSize);
 	
 	//PAPI measurements
-	PAPI_read_counters(counters, 2);
-	printf("%lld L1 cache misses (%.3lf%% misses)\n", counters[0],(double)counters[0] / (double)counters[1]);
+	// PAPI_read_counters(counters, 2);
+	// printf("%lld L1 cache misses (%.3lf%% misses)\n", counters[0],(double)counters[0] / (double)counters[1]);
+	execTime=PAPI_flops(&real_time, &proc_time, &flpins, &mflops);
+	printf("proc_time: %f\n", proc_time);
+	PAPI_shutdown();
 
 	//CPUID to flush pipeline and serialize instructions
 	int x, y;
